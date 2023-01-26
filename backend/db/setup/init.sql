1068d4e63404554b73070a61d604d02e412b93d9
gavinmorrow <gavinfmorrow@gmail.com>
2023-01-26 18:44:21 -0500

    Update init.sql

--- backend/db/setup/init.sql
+++ backend/db/setup/init.sql
@@ -10,7 +10,7 @@ BEGIN;
 
 CREATE TABLE IF NOT EXISTS users (
   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-  username VARCHAR(255) NOT NULL UNIQUE,
+  displayName VARCHAR(255) NOT NULL UNIQUE,
   email VARCHAR(255) NOT NULL,
   password VARCHAR(255) NOT NULL
 );
