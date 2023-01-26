31782b10f600e519c800e093efc0c4f6c3cc6d45
gavinmorrow <gavinfmorrow@gmail.com>
2023-01-26 15:33:20 -0500

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 
-const db = require("./db/db");
+// Generate JWT token secret
+const secret = crypto.randomBytes(64).toString("hex");
+process.env.ACCESS_TOKEN_SECRET = secret;
 
 app.get("/", (_req, res) => res.send("Hello World!"));
 
