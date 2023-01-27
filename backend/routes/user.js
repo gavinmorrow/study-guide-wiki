960ec60d01eed008ef719d44f69fcfc9568563a7
gavinmorrow <gavinfmorrow@gmail.com>
2023-01-27 16:22:27 -0500

    Squash bugs
    - Actually wait for the db request to finish.
    - Strip the password out (even though its hashed, it shouldn't be sent).

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,19 +2,25 @@ const db = require("../db/db");
 const authenticate = require("./authenticate");
 
 /** A route to get all the info of a user.  */
-const user = (req, res) => {
+const user = async (req, res) => {
     // Get the id
     const id = req.userId;
     if (id == null) return res.status(400).send("No id provided.");
 
     // Get the user from the database
-    const user = db.users.get(id);
+    const user = await db.users.get(id);
 
     // If the user doesn't exist, return a 404
     if (user == null) return res.status(404).send("User not found.");
 
-    // Return the user
-    res.json(user);
+    // Get the JSON
+    const userJson = user.toJSON();
+
+    // Strip the password
+    delete userJson.password;
+
+    // Send the JSON
+    res.json(userJson);
 };
 
 module.exports = [authenticate, user];
