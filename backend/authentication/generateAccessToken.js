872bd7c1c4e269b081d5af0d555e69e4afa55148
gavinmorrow <gavinfmorrow@gmail.com>
2023-01-26 19:57:25 -0500

    Update documentation

--- backend/authentication/generateAccessToken.js
+++ backend/authentication/generateAccessToken.js
@@ -1,17 +1,17 @@
 const jwt = require("jsonwebtoken");
 
 /**
- * Creates a token with the given payload.
- * @param {string | object | buffer } payload The data to encode in the JWT.
+ * Creates a token for a user.
+ * @param {string} id The id of the user to generate an access token for.
  * @returns {string} The token.
  */
-const generateAccessToken = payload => {
+const generateAccessToken = id => {
     // Ensure that the ACCESS_TOKEN_SECRET environment variable is set
     if (!process.env.ACCESS_TOKEN_SECRET) {
         throw new Error("ACCESS_TOKEN_SECRET environment variable is not set");
     }
 
-    const token = jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, {
+    const token = jwt.sign(id, process.env.ACCESS_TOKEN_SECRET, {
         expiresIn: "30m",
     });
     return token;
