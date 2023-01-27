d76967fb8bd40261b5e0f8d160a9191416822843
gavinmorrow <gavinfmorrow@gmail.com>
2023-01-26 20:42:14 -0500

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -3,10 +3,12 @@ const app = express();
 const PORT = 8080;
 
 // Generate JWT token secret
+const crypto = require("crypto");
 const secret = crypto.randomBytes(64).toString("hex");
 process.env.ACCESS_TOKEN_SECRET = secret;
 
 app.get("/", (_req, res) => res.send("Hello World!"));
 app.post("/login", require("./routes/login"));
+app.post("/signup", require("./routes/signup"));
 
 app.listen(PORT, () => console.log(`Listening on localhost:${PORT}!`));
