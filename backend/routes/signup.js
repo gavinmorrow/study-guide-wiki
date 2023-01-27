650ec6937ac72023f34845109ba5800869cd3a55
gavinmorrow <gavinfmorrow@gmail.com>
2023-01-26 19:59:04 -0500

    Create signup.js

---
+++ backend/routes/signup.js
@@ -0,0 +1,34 @@
+const generateAccessToken = require("../authentication/generateAccessToken");
+const db = require("../db/db");
+
+/** A route to create a new user. */
+const signup = async (req, res) => {
+    const { password } = req.body;
+
+    // Ensure that the password is valid
+    if (password == null) res.status(400).send("Password is required");
+
+    // Generate a random ID for the user
+    /**
+     * A random id for the user.
+     * @type {string}
+     */
+    let id;
+
+    // Ensure that the id is unique
+    do {
+        id = crypto.randomUUID();
+    } while ((await db.users.get(id)) != null);
+
+    // Create the user
+    const user = new User(id, password);
+    await db.users.add(user);
+
+    // Generate a JWT token
+    const token = generateAccessToken(user.id);
+
+    // Send the token to the user
+    res.send(token);
+};
+
+module.exports = signup;
