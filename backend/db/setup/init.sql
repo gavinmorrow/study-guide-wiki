af136f0010460eea57ffbf84268815fc9ecca0f2
gavinmorrow <gavinfmorrow@gmail.com>
2023-01-27 15:42:47 -0500

    Add `displayName` column to database

--- backend/db/setup/init.sql
+++ backend/db/setup/init.sql
@@ -10,7 +10,8 @@ BEGIN;
 
 CREATE TABLE IF NOT EXISTS users (
   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-  password VARCHAR(255) NOT NULL
+  password VARCHAR(255) NOT NULL,
+  displayName VARCHAR(255) UNIQUE NOT NULL,
 );
 
 COMMIT;
