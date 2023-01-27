67251fd2fdd414ea1898c4dad285d586945ce3ce
gavinmorrow <gavinfmorrow@gmail.com>
2023-01-27 09:41:52 -0500

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -7,9 +7,15 @@ const crypto = require("crypto");
 const secret = crypto.randomBytes(64).toString("hex");
 process.env.ACCESS_TOKEN_SECRET = secret;
 
-// Routes
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (_req, res) => res.send("Hello World!"));
 app.post("/login", require("./routes/login"));
 app.post("/signup", require("./routes/signup"));
+app.get("/protected", require("./routes/authenticate"), (_req, res) => {
+    res.send("You are authenticated!");
+});
 
 app.listen(PORT, () => console.log(`Listening on localhost:${PORT}!`));
