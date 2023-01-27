bdaf9c619c4836beccce3ba2477c3a09477005a1
gavinmorrow <gavinfmorrow@gmail.com>
2023-01-26 19:46:52 -0500

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -7,5 +7,6 @@ const secret = crypto.randomBytes(64).toString("hex");
 process.env.ACCESS_TOKEN_SECRET = secret;
 
 app.get("/", (_req, res) => res.send("Hello World!"));
+app.post("/login", require("./routes/login"));
 
 app.listen(PORT, () => console.log(`Listening on localhost:${PORT}!`));
