4f7c342b7e319d40ffb6ee0b57c6406e79fbb06b
gavinmorrow <gavinfmorrow@gmail.com>
2023-01-27 11:29:03 -0500

    automatic reuse detection for refresh tokens

--- backend/routes/refresh.js
+++ backend/routes/refresh.js
@@ -2,6 +2,16 @@ const jwt = require("jsonwebtoken");
 const generateAccessToken = require("../authentication/generateAccessToken");
 const generateRefreshToken = require("../authentication/generateRefreshToken");
 
+// Each refresh token is only valid for one use
+// And whenever an old token is used, the entire family of tokens is invalidated
+// Each family is represented by a UUID
+
+/** @type {Set<string>} Raw refresh tokens (JWTs) */
+let usedTokens = Set();
+
+/** @type {Set<string>} UUIDs */
+let invalidFamilies = Set();
+
 /** A route to refresh a JWT. It takes a refresh token and returns a new access token and refresh token. */
 const refresh = async (req, res) => {
     // Get the refresh token from the request body
@@ -11,22 +21,48 @@ const refresh = async (req, res) => {
     }
 
     // Verify the refresh token
-    let oldRefreshToken;
+    let id, family;
     try {
-        oldRefreshToken = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET);
+        const token = jwt.verify(
+            refreshToken,
+            process.env.REFRESH_TOKEN_SECRET
+        );
+
+        id = token.id;
+        family = token.family;
+
+        if (id == null || family == null) {
+            console.error("Invalid refresh token:", token);
+            return res.status(401).send("Invalid refresh token");
+        }
     } catch (err) {
         if (!(err instanceof jwt.JsonWebTokenError)) {
+            res.sendStatus(500);
             throw err;
-		}
-		
+        }
+
         return res.status(401).send("Invalid refresh token");
     }
 
+    // Check if the refresh token family has been invalidated
+    if (invalidFamilies.has(family)) {
+        res.status(401).send("Invalid refresh token");
+    }
+
+    // If the token has been used before, invalidate the entire family
+    if (usedTokens.has(refreshToken)) {
+        invalidFamilies.add(family);
+        return res.status(401).send("Invalid refresh token");
+    }
+
+    // Invalidate the old refresh token
+    usedTokens.add(refreshToken);
+
     // Generate a new access token
-    const accessToken = generateAccessToken(oldRefreshToken.id);
+    const accessToken = generateAccessToken(id);
 
     // Generate a new refresh token
-    const newRefreshToken = generateRefreshToken(oldRefreshToken.id);
+    const newRefreshToken = generateRefreshToken(id, family);
 
     // Return the new access token and refresh token to the user
     res.json({ accessToken, refreshToken: newRefreshToken });
