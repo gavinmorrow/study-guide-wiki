f40b3c8a83e4640303e33da4caaea8772c1bd11b
gavinmorrow <gavinfmorrow@gmail.com>
2023-02-02 20:42:46 -0500

    Add guides to db

--- backend/db/setup/init.sql
+++ backend/db/setup/init.sql
@@ -15,4 +15,31 @@ CREATE TABLE IF NOT EXISTS users (
   displayName VARCHAR(255) UNIQUE NOT NULL
 );
 
+CREATE TABLE IF NOT EXISTS guides (
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  title VARCHAR(255) NOT NULL,
+  description VARCHAR(255) NOT NULL DEFAULT '',
+  authorId UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+  grade INT NOT NULL,
+  subject VARCHAR(255) NOT NULL,
+  teacher VARCHAR(255) NOT NULL,
+  year INT NOT NULL
+);
+
+CREATE TYPE permissions AS ENUM ('none', 'read', 'suggest', 'edit', 'manage', 'owner');
+
+CREATE TABLE IF NOT EXISTS user_guides (
+  -- The user who is a part of the guide. 
+  -- To get all the users who are a part of the guide, query the 
+  -- `userId` column for the `guideId` of the guide you want.
+  userId UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+  guideId UUID NOT NULL REFERENCES guides(id) ON DELETE CASCADE,
+
+  -- The permissions the user has for the guide.
+  -- If you have one, you have all the permissions below it (with a lower value)
+  permissions permissions NOT NULL DEFAULT 'none',
+
+  PRIMARY KEY (userId, guideId)
+);
+
 COMMIT;
