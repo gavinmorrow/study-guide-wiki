826dac30d9069a707da9dc0afa624ad87aa177b0
gavinmorrow <gavinfmorrow@gmail.com>
2023-02-06 17:04:52 -0500

    Indent w/ tabs

--- backend/routes/guide/guide.js
+++ backend/routes/guide/guide.js
@@ -1,7 +1,14 @@
-const GET_guide = (req, res) => {
-    const guideId = req.params.id;
+const db = require("../../db/db");
+
+/**
+	* A route to fetch the data for a guide.
+	* @param {import("express").Request} req The request object.
+	* @param {import("express").Response} res The response object.
+	*/
+const GET_guide = async (req, res) => {
+	const guideId = req.params.id;
 	const userId = req.userId;
-	
+
 	if (!guideId) {
 		return res.sendStatus(400);
 	}
@@ -13,20 +20,20 @@ const PUT_guide = (req, res) => {};
 
 const DELETE_guide = (req, res) => {};
 
-const ALL_guide = (req, res) => {
-    const method = req.method;
-    switch (method) {
-        case "GET":
-            return GET_guide(req, res);
-        case "POST":
-            return POST_guide(req, res);
-        case "PUT":
-            return PUT_guide(req, res);
-        case "DELETE":
-            return DELETE_guide(req, res);
-        default:
-            return res.sendStatus(405);
-    }
+const ALL_guide = async (req, res) => {
+	const method = req.method;
+	switch (method) {
+		case "GET":
+			return await GET_guide(req, res);
+		case "POST":
+			return POST_guide(req, res);
+		case "PUT":
+			return PUT_guide(req, res);
+		case "DELETE":
+			return DELETE_guide(req, res);
+		default:
+			return res.sendStatus(405);
+	}
 };
 
 module.exports = guide;
