6c19eebabbd48533f4d0451b63dc3272f19d175a
gavinmorrow <gavinfmorrow@gmail.com>
2023-02-08 06:35:47 -0500

    Use tabs (not spaces)

--- backend/routes/user/login.js
+++ backend/routes/user/login.js
@@ -4,25 +4,25 @@ const generateRefreshToken = require("../../authentication/generateRefreshToken"
 
 /** A route to login a user and return a JWT. */
 const login = async (req, res) => {
-    // Get the username and password from the request body
-    const { id, password } = req.body;
-    if (!id || !password) {
-        return res.status(400).send("Id and password required");
-    }
+	// Get the username and password from the request body
+	const { id, password } = req.body;
+	if (!id || !password) {
+		return res.status(400).send("Id and password required");
+	}
 
-    // Check if the id and password are correct
-    if (!credentialsAreValid(id, password)) {
-        return res.status(401).send("Invalid credentials");
-    }
+	// Check if the id and password are correct
+	if (!(await credentialsAreValid(id, password))) {
+		return res.status(401).send("Invalid credentials");
+	}
 
-    // Generate an access token with the id as the payload
-    const accessToken = generateAccessToken(id);
+	// Generate an access token with the id as the payload
+	const accessToken = generateAccessToken(id);
 
-    // Generate a refresh token with the id as the payload
-    const refreshToken = generateRefreshToken(id);
+	// Generate a refresh token with the id as the payload
+	const refreshToken = generateRefreshToken(id);
 
-    // Return the token to the user
-    res.json({ accessToken, refreshToken });
+	// Return the token to the user
+	res.json({ accessToken, refreshToken });
 };
 
 module.exports = login;
