712fc0c7d65da077a0df5645d383abf24faaf8ac
gavinmorrow <gavinfmorrow@gmail.com>
2023-02-08 06:36:22 -0500

    Fix names to use snake case (not camelCase)

--- backend/db/setup/init.sql
+++ backend/db/setup/init.sql
@@ -12,34 +12,34 @@ BEGIN;
 CREATE TABLE IF NOT EXISTS users (
   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
   password VARCHAR(255) NOT NULL,
-  displayName VARCHAR(255) UNIQUE NOT NULL
+  display_name VARCHAR(255) UNIQUE NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS guides (
   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
   title VARCHAR(255) NOT NULL,
   description VARCHAR(255) NOT NULL DEFAULT '',
-  authorId UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+  owner_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
   grade INT NOT NULL,
   subject VARCHAR(255) NOT NULL,
   teacher VARCHAR(255) NOT NULL,
   year INT NOT NULL
 );
 
-CREATE TYPE permissions AS ENUM ('read', 'suggest', 'edit', 'manage');
+CREATE TYPE permission_levels AS ENUM ('read', 'suggest', 'edit', 'manage');
 
-CREATE TABLE IF NOT EXISTS guideAccess (
+CREATE TABLE IF NOT EXISTS guide_access (
   -- The user who is a part of the guide. 
   -- To get all the users who are a part of the guide, query the 
   -- `userId` column for the `guideId` of the guide you want.
-  userId UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-  guideId UUID NOT NULL REFERENCES guides(id) ON DELETE CASCADE,
+  user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+  guide_id UUID NOT NULL REFERENCES guides(id) ON DELETE CASCADE,
 
   -- The permissions the user has for the guide.
   -- If you have one, you have all the permissions below it (with a lower value)
-  permissions permissions NOT NULL,
+  permission_level permission_levels NOT NULL,
 
-  PRIMARY KEY (userId, guideId)
+  PRIMARY KEY (user_id, guide_id)
 );
 
 COMMIT;
