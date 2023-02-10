a0e76839a811fc29dc3c2437784d7647c3d24e41
gavinmorrow <gavinfmorrow@gmail.com>
2023-02-10 16:24:35 -0500

    Change how secrets are generated in dev and prod
    In dev, when the server was restarted (e.g. because of a file change), new jwt secrets were generated, requiring everyone to login again.
    
    That isn't really a problem in production, but in dev it was annoying.
    
    So, now it only regenerates secrets in production. In development, it uses predefined, insecure, secrets.
    
    This will not affect anything else.

--- backend/app.js
+++ backend/app.js
@@ -4,8 +4,13 @@ const PORT = 8080;
 
 // Generate JWT token secrets
 const crypto = require("crypto");
-process.env.ACCESS_TOKEN_SECRET = crypto.randomBytes(64).toString("hex");
-process.env.REFRESH_TOKEN_SECRET = crypto.randomBytes(64).toString("hex");
+const dev = process.env.NODE_ENV === "development";
+process.env.ACCESS_TOKEN_SECRET = dev
+	? "access"
+	: crypto.randomBytes(64).toString("hex");
+process.env.REFRESH_TOKEN_SECRET = dev
+	? "refresh"
+	: crypto.randomBytes(64).toString("hex");
 console.log("Access token secret:", process.env.ACCESS_TOKEN_SECRET);
 console.log("Refresh token secret:", process.env.REFRESH_TOKEN_SECRET);
 
