2b576eb8f0e87a1155ca88d12b7e860fa0b49871
gavinmorrow <gavinfmorrow@gmail.com>
2023-02-23 07:17:13 -0700

    Create guideEditorHeader.js

---
+++ frontend/public/js/guideEditorHeader.js
@@ -0,0 +1,28 @@
+const changeTitle = async newTitle => {
+	const request = (await import("./methods/request.js")).default;
+	console.log(`Changing title to ${newTitle}`);
+
+	const id = document
+		.querySelector("meta[name='data-guide-id']")
+		.getAttribute("content");
+
+	const response = await request("/guide", "PATCH", {
+		id,
+		newTitle,
+	});
+	if (!response.ok) {
+		console.error("Failed to change title");
+		return;
+	}
+};
+
+const titleInput = document.getElementById("guide-title-input");
+
+// Timeouts so it doesn't send a request every time the user types a letter
+let lastChangeTimeout;
+titleInput.addEventListener("input", () => {
+	clearTimeout(lastChangeTimeout);
+	lastChangeTimeout = setTimeout(() => {
+		changeTitle(titleInput.value);
+	}, 1000);
+});
