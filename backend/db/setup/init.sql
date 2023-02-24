70da51a3b856c4616d8123c854ee9437820a3caa
gavinmorrow <gavinfmorrow@gmail.com>
2023-02-23 20:55:21 -0700

    Update init.sql

--- backend/db/setup/init.sql
+++ backend/db/setup/init.sql
@@ -40,7 +40,9 @@ CREATE TABLE IF NOT EXISTS guide_access (
 CREATE TABLE IF NOT EXISTS guide_sections (
   guide_id UUID NOT NULL REFERENCES guides(id) ON DELETE CASCADE,
   title VARCHAR(255) NOT NULL,
-  content TEXT NOT NULL
+  content TEXT NOT NULL,
+
+  PRIMARY KEY (guide_id, title)
 );
 
 COMMIT;
