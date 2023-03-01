be1a73418cf5bc3d803a4ffc8ff8a6abc5e93949
gavinmorrow <gavinfmorrow@gmail.com>
2023-03-01 16:13:20 -0500

    Remove example ws code for guide editor frontent

--- frontend/public/js/guideEditor.js
+++ frontend/public/js/guideEditor.js
@@ -1,15 +1,7 @@
+const guideId = document.getElementById("data-guide-id").value;
+
 // Connect to the server with websockets
-const socket = new WebSocket("ws://localhost:8080/echo");
-socket.addEventListener("open", () => {
-	console.log("Connected to the server");
-	socket.send("Ping!");
-});
-socket.addEventListener("message", event => {
-	console.log("Message from the server:", event.data);
-});
-socket.addEventListener("error", event => {
-	console.log("Error from the server", event.data);
-});
+const socket = new WebSocket(`ws://localhost:8080/api/guide/${guideId}`);
 
 const editor = document.getElementById("editor");
 editor.addEventListener("input", () => {
