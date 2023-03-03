4940c1a79acd85ac2cd8519dc67dec130b6cd418
gavinmorrow <gavinfmorrow@gmail.com>
2023-03-03 07:47:59 -0500

    Update WSMessage.js

--- backend/client/classes/WSMessage.js
+++ backend/client/classes/WSMessage.js
@@ -2,14 +2,19 @@
  * @typedef {string} WSMessageType
  * @enum {WSMessageType}
  * @property {WSMessageType} PING
+ * @property {WSMessageType} PONG
+ * @property {WSMessageType} UPDATE
  * @property {WSMessageType} ERROR
  */
 
+const types = ["PING", "PONG", "UPDATE", "ERROR"].reduce((acc, type) => {
+	acc[type] = Symbol(type);
+	return acc;
+}, {});
+
 class WSMessage {
 	static get types() {
-		return {
-			ERROR: "error",
-		};
+		types;
 	}
 
 	constructor(type, data) {
