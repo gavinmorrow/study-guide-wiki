846192e3a811936130f5512e332b32fd7c4c7157
gavinmorrow <gavinfmorrow@gmail.com>
2023-03-04 07:25:53 -0500

    Update guideEditor.js

--- backend/client/routes/guideEditor.js
+++ backend/client/routes/guideEditor.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 
 const db = require("../../db/db");
+const logger = require("../../logger");
+const WSMessage = require("../classes/WSMessage");
 
 router.get("/:id", async (req, res) => {
 	// Get guide
@@ -18,18 +20,54 @@ router.get("/:id", async (req, res) => {
 	});
 });
 
-// Websocket api
-router.ws("/:id", async (ws, req) => {
-	logger.mark(`Websocket editor connected to guide ${req.params.id}`);
-	ws.on("message", msg => {
-		if (msg.type == null)
-			ws.send({
-				type: "error",
-				message: "Invalid message type",
-			});
+const handleMessage = (msg, ws) => {
+	logger.info("Message from the client:", msg);
 
-		logger.info("Message from the client:", msg);
+	switch (msg.type) {
+		case "ping":
+			ws.send(WSMessage.pong());
+			break;
+
+		case "update":
+			break;
+
+		case "error":
+			break;
+
+		default:
+			ws.send(WSMessage.error("Invalid message type", msg.type));
+	}
+};
+
+router.ws("/:id", (ws, req) => {
+	logger.debug(`Websocket editor connected to guide ${req.params.id}`);
+
+	ws.on("message", msg => {
+		try {
+			msg = JSON.parse(msg);
+		} catch (e) {
+			ws.send(
+				WSMessage.error(
+					"Invalid message format. Message must be JSON.",
+					msg
+				)
+			);
+			logger.debug("Invalid message format for websocket message:", msg);
+		}
+
+		if (msg.type == null)
+			ws.send(WSMessage.error("Message type not specified", msg));
+
+		handleMessage(msg, ws);
 	});
+
+	ws.on("close", () => {
+		logger.info(
+			`Websocket editor disconnected from guide ${req.params.id}`
+		);
+	});
+
+	ws.send("Hi!");
 });
 
 module.exports = router;
