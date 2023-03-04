ab6760254bd5a0a6484eff859701b1c2f0818ddd
gavinmorrow <gavinfmorrow@gmail.com>
2023-03-04 07:25:51 -0500

    Update WSMessage.js

--- backend/client/classes/WSMessage.js
+++ backend/client/classes/WSMessage.js
@@ -7,17 +7,61 @@
  * @property {WSMessageType} ERROR
  */
 
-const types = ["PING", "PONG", "UPDATE", "ERROR"].reduce((acc, type) => {
-	acc[type] = Symbol(type);
-	return acc;
-}, {});
-
+/**
+ * A message to be sent over the WebSocket connection.
+ */
 class WSMessage {
-	static get types() {
-		types;
+	static types = {
+		ping: "ping",
+		pong: "pong",
+		update: "update",
+		error: "error",
+	};
+
+	/**
+	 * Creates a new ping message.
+	 * @returns {string}
+	 */
+	static ping() {
+		return new WSMessage(WSMessage.types.ping).toString();
 	}
 
-	constructor(type, data) {
+	/**
+	 * Creates a new pong message.
+	 * @returns {string}
+	 */
+	static pong() {
+		return new WSMessage(WSMessage.types.pong).toString();
+	}
+
+	/**
+	 * Creates a new update message.
+	 * @param {any} data The data to send.
+	 * @returns {string}
+	 */
+	static update(data) {
+		return new WSMessage(WSMessage.types.update, data).toString();
+	}
+
+	/**
+	 * Creates a new error message.
+	 * @param {string} message The error message.
+	 * @param {any} data The data to send.
+	 * @returns {string}
+	 */
+	static error(message, data = null) {
+		return new WSMessage(WSMessage.types.error, {
+			message,
+			data,
+		}).toString();
+	}
+
+	/**
+	 * Creates a new WSMessage.
+	 * @param {WSMessageType} type The type of the message.
+	 * @param {any} data The data of the message.
+	 */
+	constructor(type, data = null) {
 		this.type = type;
 		this.data = data;
 	}
@@ -27,4 +71,23 @@ class WSMessage {
 	 * @type {WSMessageType}
 	 */
 	type;
+
+	/**
+	 * The data of the message.
+	 * @type {any}
+	 */
+	data;
+
+	toJSON() {
+		return {
+			type: this.type,
+			data: this.data,
+		};
+	}
+
+	toString() {
+		return JSON.stringify(this.toJSON());
+	}
 }
+
+module.exports = WSMessage;
