043903e5728320fd8683d4861a9d57fd2069d180
gavinmorrow <gavinfmorrow@gmail.com>
2023-03-04 07:25:56 -0500

    Update guideEditor.js

--- frontend/public/js/guideEditor.js
+++ frontend/public/js/guideEditor.js
@@ -1,7 +1,20 @@
-const guideId = document.getElementById("data-guide-id").value;
+const guideId = document.querySelector("meta[name='data-guide-id']").content;
 
 // Connect to the server with websockets
-const socket = new WebSocket(`ws://localhost:8080/api/guide/${guideId}`);
+const socket = new WebSocket(`ws://localhost:8080/guide/${guideId}`);
+socket.addEventListener("open", () => {
+	console.log("Connected to the server");
+
+	socket.send(JSON.stringify({ type: "ping" }));
+});
+socket.addEventListener("message", msg => {
+	console.log("Message from the server:", msg);
+});
+socket.addEventListener("error", err => {
+	console.error("Error from the server:", err);
+});
+console.log(socket);
+setInterval(() => socket.send(JSON.stringify({ type: "ping" })), 1000 * 60);
 
 const editor = document.getElementById("editor");
 editor.addEventListener("input", () => {
