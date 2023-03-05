aedbe732d37be17d693342b166fc423a0cc76cc0
gavinmorrow <gavinfmorrow@gmail.com>
2023-03-05 09:21:52 -0500

    Update guideEditor.js

--- frontend/public/js/guideEditor.js
+++ frontend/public/js/guideEditor.js
@@ -4,17 +4,15 @@ const guideId = document.querySelector("meta[name='data-guide-id']").content;
 const socket = new WebSocket(`ws://localhost:8080/guide/${guideId}`);
 socket.addEventListener("open", () => {
 	console.log("Connected to the server");
-
-	socket.send(JSON.stringify({ type: "ping" }));
 });
+
 socket.addEventListener("message", msg => {
 	console.log("Message from the server:", msg);
 });
+
 socket.addEventListener("error", err => {
 	console.error("Error from the server:", err);
 });
-console.log(socket);
-setInterval(() => socket.send(JSON.stringify({ type: "ping" })), 1000 * 60);
 
 const editor = document.getElementById("editor");
 editor.addEventListener("input", () => {
