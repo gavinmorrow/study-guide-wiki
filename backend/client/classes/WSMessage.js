a7b55f5f6632e49b85e4cf397fd6890ac882b9ab
gavinmorrow <gavinfmorrow@gmail.com>
2023-03-07 18:37:40 -0500

    add more types of wsmessage

--- backend/client/classes/WSMessage.js
+++ backend/client/classes/WSMessage.js
@@ -3,8 +3,28 @@
  * @enum {WSMessageType}
  * @property {WSMessageType} PING
  * @property {WSMessageType} PONG
+ *
  * @property {WSMessageType} UPDATE
+ *
+ * @property {WSMessageType} LOCK_PARAGRAPH
+ * @property {WSMessageType} UNLOCK_PARAGRAPH
+ *
  * @property {WSMessageType} ERROR
+ */
+
+/**
+ * @typedef {string} UpdateType
+ * @enum {UpdateType}
+ *
+ * @property {UpdateType} UPDATE_GUIDE_TITLE
+ *
+ * @property {UpdateType} NEW_SECTION
+ * @property {UpdateType} UPDATE_SECTION_TITLE
+ * @property {UpdateType} DELETE_SECTION
+ *
+ * @property {UpdateType} NEW_PARAGRAPH
+ * @property {UpdateType} UPDATE_PARAGRAPH
+ * @property {UpdateType} DELETE_PARAGRAPH
  */
 
 /**
@@ -14,7 +34,17 @@ class WSMessage {
 	static types = {
 		ping: "ping",
 		pong: "pong",
-		update: "update",
+		update: {
+			updateGuideTitle: "updateGuideTitle",
+			newSection: "newSection",
+			updateSectionTitle: "updateSectionTitle",
+			deleteSection: "deleteSection",
+			newParagraph: "newParagraph",
+			updateParagraph: "updateParagraph",
+			deleteParagraph: "deleteParagraph",
+		},
+		lockParagraph: "lockParagraph",
+		unlockParagraph: "unlockParagraph",
 		error: "error",
 	};
 
@@ -36,11 +66,44 @@ class WSMessage {
 
 	/**
 	 * Creates a new update message.
+	 * @param {UpdateType} type The type of the update.
 	 * @param {any} data The data to send.
+	 * @returns {string?} The message, or null if the type is invalid.
+	 */
+	static update(type, data) {
+		const type = WSMessage.types.update[type];
+		if (type == null) {
+			logger.error(`Invalid update type: ${type}`);
+			return null;
+		}
+
+		return new WSMessage(type, data).toString();
+	}
+
+	/**
+	 * Creates a new lock paragraph message. This *does not* check if the paragraph is already locked, nor does it actually lock the paragraph.
+	 * @param {string} paragraphId The ID of the paragraph to lock.
+	 * @param {string} userId The ID of the user who locked the paragraph.
 	 * @returns {string}
 	 */
-	static update(data) {
-		return new WSMessage(WSMessage.types.update, data).toString();
+	static lockParagraph(paragraphId, userId) {
+		return new WSMessage(WSMessage.types.lockParagraph, {
+			paragraphId,
+			userId,
+		}).toString();
+	}
+
+	/**
+	 * Creates a new unlock paragraph message. This *does not* check if the paragraph is already unlocked, nor does it actually unlock the paragraph.
+	 * @param {string} paragraphId The ID of the paragraph to unlock.
+	 * @param {string} userId The ID of the user who unlocked the paragraph.
+	 * @returns {string}
+	 */
+	static unlockParagraph (paragraphId, userId) {
+		return new WSMessage(WSMessage.types.unlockParagraph, {
+			paragraphId,
+			userId,
+		}).toString();
 	}
 
 	/**
