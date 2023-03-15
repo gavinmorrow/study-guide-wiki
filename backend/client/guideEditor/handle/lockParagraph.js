08f27d899fb09159d1a01f258307955b7b414d62
gavinmorrow <gavinfmorrow@gmail.com>
2023-03-15 18:56:26 -0400

    Update lockParagraph for `UserSession`

--- backend/client/guideEditor/handle/lockParagraph.js
+++ backend/client/guideEditor/handle/lockParagraph.js
@@ -1,13 +1,22 @@
 const logger = require("../../../logger");
+const Session = require("../../classes/Session");
+const UserSession = require("../../classes/UserSession");
 const WSMessage = require("../../classes/WSMessage");
-const lockParagraph = (ws, data, session) => {
-	logger.trace(`Locking paragraph ${data.paragraphId} for user ${ws.userId}.`);
 
-	if (session.locks.find(lock => lock.userId === ws.userId)) {
+/**
+ * Locks a paragraph for a user.
+ * @param {WebSocket} ws The user's websocket.
+ * @param {object} data The data sent by the client.
+ * @param {string} data.paragraphId The paragraph UUID.
+ * @param {UserSession} userSession The user's session data.
+ * @param {Session} session The session data.
+ */
+const lockParagraph = (ws, data, userSession, session) => {
+	logger.trace(`Locking paragraph ${data.paragraphId} for user ${userSession.userId}.`);
+
+	if (session.locks.find(lock => lock.token === userSession.token)) {
 		logger.warn(
-			"User",
-			ws.userId,
-			"tried to lock a paragraph while already having a lock. Ignoring."
+			`Session token ${userSession.token} already has a lock on a paragraph. Ignoring lock request.`
 		);
 		ws.send(
 			WSMessage.error("You already have a lock on a paragraph.", {
@@ -18,6 +27,6 @@ const lockParagraph = (ws, data, session) => {
 		return;
 	}
 
-	session.locks.push({ userId: ws.userId, paragraphId: data.paragraphId });
+	session.locks.push({ token: userSession.token, paragraphId: data.paragraphId });
 };
 module.exports = lockParagraph;
