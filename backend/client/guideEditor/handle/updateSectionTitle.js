cfa3fca8a8955e61c41be9dd468203c9aad19e2c
gavinmorrow <gavinfmorrow@gmail.com>
2023-03-16 20:53:44 -0400

    Update updateSectionTitle.js

--- backend/client/guideEditor/handle/updateSectionTitle.js
+++ backend/client/guideEditor/handle/updateSectionTitle.js
@@ -1,5 +1,15 @@
 const logger = require("../../../logger");
-const updateSectionTitle = (ws, data, session) => {
+const Session = require("../../classes/Session");
+const UserSession = require("../../classes/UserSession");
+
+/**
+ * Updates the section title.
+ * @param {object} data The message data.
+ * @param {string} data.newValue The new section title.
+ * @param {UserSession} userSession The user's session data.
+ * @param {Session} session The session data.
+ */
+const updateSectionTitle = (data, userSession, session) => {
 	logger.trace("Updating section title", data);
 
 	// TODO: update section title
