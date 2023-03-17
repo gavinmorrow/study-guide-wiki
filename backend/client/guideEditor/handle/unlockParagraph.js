2beb151b9d47f914dc35f918e9cd0943b6c51fff
gavinmorrow <gavinfmorrow@gmail.com>
2023-03-16 20:53:42 -0400

    Update unlockParagraph.js

--- backend/client/guideEditor/handle/unlockParagraph.js
+++ backend/client/guideEditor/handle/unlockParagraph.js
@@ -1,17 +1,20 @@
 const logger = require("../../../logger");
 const Session = require("../../classes/Session");
 const UserSession = require("../../classes/UserSession");
+const WSMessage = require("../../classes/WSMessage");
 
 /**
  * Unlocks a paragraph for a user.
- * @param {WebSocket} ws The user's websocket.
  * @param {object} data The message data.
  * @param {UserSession} userSession The user's session data.
  * @param {Session} session The session data.
  */
-const unlockParagraph = (ws, data, userSession, session) => {
+const unlockParagraph = (data, userSession, session) => {
 	logger.trace("Unlocking paragraph for session token", userSession.token);
 
 	session.locks = session.locks.filter(lock => lock.token !== userSession.token);
+
+	// broadcast the unlock to all users
+	
 };
 module.exports = unlockParagraph;
