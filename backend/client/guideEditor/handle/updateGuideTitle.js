c82ba6d69e45104d4d7434a115d8616413c01e6e
gavinmorrow <gavinfmorrow@gmail.com>
2023-03-16 20:53:43 -0400

    Update updateGuideTitle.js

--- backend/client/guideEditor/handle/updateGuideTitle.js
+++ backend/client/guideEditor/handle/updateGuideTitle.js
@@ -1,5 +1,15 @@
 const logger = require("../../../logger");
-const updateGuideTitle = (ws, data, session) => {
+const Session = require("../../classes/Session");
+const UserSession = require("../../classes/UserSession");
+
+/**
+ * Updates the guide title.
+ * @param {object} data The message data.
+ * @param {string} data.title The new guide title.
+ * @param {UserSession} userSession The user's session data.
+ * @param {Session} session The session data.
+ */
+const updateGuideTitle = (data, userSession, session) => {
 	logger.trace("Updating guide title", data);
 
 	// TODO: update guide title
