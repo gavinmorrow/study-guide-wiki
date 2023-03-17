14df9e17089cbe91a4d423951cd8a62ea853a155
gavinmorrow <gavinfmorrow@gmail.com>
2023-03-16 20:53:39 -0400

    Update lockParagraph.js

--- backend/client/guideEditor/handle/lockParagraph.js
+++ backend/client/guideEditor/handle/lockParagraph.js
@@ -5,20 +5,19 @@ const WSMessage = require("../../classes/WSMessage");
 
 /**
  * Locks a paragraph for a user.
- * @param {WebSocket} ws The user's websocket.
  * @param {object} data The data sent by the client.
  * @param {string} data.paragraphId The paragraph UUID.
  * @param {UserSession} userSession The user's session data.
  * @param {Session} session The session data.
  */
-const lockParagraph = (ws, data, userSession, session) => {
+const lockParagraph = (data, userSession, session) => {
 	logger.trace(`Locking paragraph ${data.paragraphId} for session token ${userSession.token}.`);
 
 	if (session.locks.find(lock => lock.token === userSession.token)) {
 		logger.warn(
 			`Session token ${userSession.token} already has a lock on a paragraph. Ignoring lock request.`
 		);
-		ws.send(
+		userSession.ws.send(
 			WSMessage.error("You already have a lock on a paragraph.", {
 				type: "alreadyLocked",
 				paragraphId: data.paragraphId,
