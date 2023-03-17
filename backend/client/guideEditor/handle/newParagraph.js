1346d14b596c964ea19e338303ed0bda37e5a66e
gavinmorrow <gavinfmorrow@gmail.com>
2023-03-16 20:53:40 -0400

    Update newParagraph.js

--- backend/client/guideEditor/handle/newParagraph.js
+++ backend/client/guideEditor/handle/newParagraph.js
@@ -1,5 +1,14 @@
 const logger = require("../../../logger");
-const newParagraph = (ws, data, session) => {
+const Session = require("../../classes/Session");
+const UserSession = require("../../classes/UserSession");
+
+/**
+ * Creates a new paragraph.
+ * @param {object} data The message data.
+ * @param {UserSession} userSession The user's session data.
+ * @param {Session} session The session data.
+ */
+const newParagraph = async (data, userSession, session) => {
 	logger.trace("New paragraph", data);
 
 	// TODO: create new paragraph in database
