d66e44b1e05ebc08704e5beb70d7cec9c9c7308a
gavinmorrow <gavinfmorrow@gmail.com>
2023-03-16 20:53:41 -0400

    Update newSection.js

--- backend/client/guideEditor/handle/newSection.js
+++ backend/client/guideEditor/handle/newSection.js
@@ -1,5 +1,16 @@
 const logger = require("../../../logger");
-const newSection = (ws, data, session) => {
+const Session = require("../../classes/Session");
+const UserSession = require("../../classes/UserSession");
+
+/**
+ * Creates a new section.
+ * @param {object} data The data sent by the client.
+ * @param {string} data.title The section title.
+ * @param {string} data.paragraphId The UUID of the paragraph to insert the section after.
+ * @param {UserSession} userSession The user's session data.
+ * @param {Session} session The session data.
+ */
+const newSection = (data, userSession, session) => {
 	logger.trace("New section", data);
 
 	// TODO: create new section
